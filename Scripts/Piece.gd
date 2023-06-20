c4e7561c75900deeb2d8261b71998a7ed3232de7
Rahul <35rahuldshetty@gmail.com>
2023-06-21 01:15:38 +0530

    added control for blocking piece move

--- Scripts/Piece.gd
+++ Scripts/Piece.gd
@@ -6,11 +6,20 @@ var file = 0
 
 var mouse_pressed
 
+var original_position;
+
+func _ready():
+	original_position = [
+		position.x, position.y
+	]
+
 func handle_mouse_press(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
 			mouse_pressed = true
 		else:
+			position.x = original_position[0]
+			position.y = original_position[1]
 			mouse_pressed = false
 
 func _process(_delta):
